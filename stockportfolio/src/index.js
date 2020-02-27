1e49972e2e037c8ccb95ace1165edff873167253
Gene Grady <grady.gene.thomas@gmail.com>
2020-02-27 09:37:54 -0500

    Added Portfolio and Transaction Pages

--- stockportfolio/src/index.js
+++ stockportfolio/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import rootReducer from './redux/rootReducer';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './App';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
