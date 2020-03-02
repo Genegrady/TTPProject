c465d4dfce86553437b96c09cac999738cbfb483
Gene Grady <grady.gene.thomas@gmail.com>
2020-03-02 09:14:59 -0500

    Styled Login and Register Form using styled components

--- stockportfolio/src/Pages/Signup.js
+++ stockportfolio/src/Pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import userActions from '../redux/actions.js';
+import styled from 'styled-components';
 
 const Signup = props => {
   // initializing dispatch
@@ -27,33 +28,76 @@ const Signup = props => {
   // Destructuring keys from our local state to use in the form
   const { name, email, password } = signupForm;
 
+  // Creating our styled components  for our form
+
+  const StyledForm = styled.form`
+  font-family: 'Open Sans Condensed', arial, sans;
+	width: 500px;
+	padding: 30px;
+	background: #FFFFFF;
+	margin: 50px auto;
+	box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+	-moz-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+	-webkit-box-shadow:  0px 0px 15px rgba(0, 0, 0, 0.22);
+  `
+  const StyledHOne = styled.h1`
+  background: #4D4D4D;
+	text-transform: uppercase;
+	font-family: 'Open Sans Condensed', sans-serif;
+	color: #797979;
+	font-size: 18px;
+	font-weight: 100;
+	padding: 20px;
+	margin: -30px -30px 30px -30px;
+  text-align: center;
+  `
+  const StyledInput = styled.input`
+  box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	outline: none;
+	display: block;
+	width: 100%;
+	padding: 7px;
+	border: none;
+	border-bottom: 1px solid #ddd;
+	background: transparent;
+	margin-bottom: 10px;
+	font: 16px Arial, Helvetica, sans-serif;
+	height: 45px;
+  `
+  const StyledSubmit = styled(StyledInput)`
+  	background:linear-gradient(to bottom, #34CACA 5%, #30C9C9 100%);
+	  background-color:#34CACA;
+  `
+
   // Component code
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Signup Page</h1>
-      <input
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledHOne>Register</StyledHOne>
+      <StyledInput
         type="text"
         name="name"
         value={name}
         onChange={handleChange}
         placeholder="Name"
       />
-      <input
+      <StyledInput
         type="text"
         name="email"
         value={email}
         onChange={handleChange}
         placeholder="Email"
       />
-      <input
+      <StyledInput
         type="password"
         name="password"
         value={password}
         onChange={handleChange}
         placeholder="Password"
       />
-      <input type="submit" />
-    </form>
+      <StyledSubmit type="submit" />
+    </StyledForm>
   );
 };
 
